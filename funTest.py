e269b348ef19ee661edc87dd306e3f3b606c7699
Tyler Skluzacek <skluzacek@uchicago.edu>
2016-11-03 20:19:27 -0500

    Pushing to VM #2

--- funTest.py
+++ funTest.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-outputfile = "/home/ubuntu/cdiac_scraper/ianTracker"
+outputfile = "/home/ubuntu/cdiac_scraper/ianTracker2"
 
 #Creates header row of output csv file
 with open(outputfile, 'a') as summary:
