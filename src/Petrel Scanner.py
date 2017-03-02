c1d68172eca740dcc93a4bcd7685496f33224ee3
tskluzac <skluzacek@uchicag.edu>
2017-03-02 15:55:16 -0600

    Deleted a bunch of crap-code.

--- src/Petrel Scanner.py
+++ src/Petrel Scanner.py
@@ -30,35 +30,3 @@ for item in df[0]:
     filename = full_path.split('/')[-1]
     filepath = full_path.split('/')[:-2]
 
-
-    # for item in filepath:
-    #     filepath =
-
-    #ftp.cwd(filename)
-    #print
-
-    #print(filename, filepath)
-
-
-
-    # try:
-    #     print("Request " + i)
-    #     req = urllib2.Request(full_path)
-    #     print("Response " + i)
-    #     response = urllib2.urlopen(req)
-    #     print("Page Access " +i)
-    #     the_page = response.read()
-    # except:
-    #     pass
-
-    i+=1
-    #urllib.urlretrieve('')
-    #print(ftp.cwd(item))
-
-
-### STEP 2: Iterate through each element
-
-    ### STEP 3: For all zip, tar, z files try extracting them into zip-folder.
-    ### STEP 4: If it fails --- pass.
-
-###STEP 5: Push to Globus.
